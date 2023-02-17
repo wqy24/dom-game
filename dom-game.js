c24f7429e260d5ab30fd532e35be2ac65a3d8adb
Woo, Chyo（伍庆耀） <113442988+wqy24@users.noreply.github.com>
2023-02-17 21:17:46 +0800

    Add Character model

--- dom-game.js
+++ dom-game.js
@@ -1,1 +1,18 @@
+function Character(imgs, x, y) {
+  this.elem = $("<img/>");
+  this.imgs = imgs;
+  $("body").append(this.elem);
+  this.x = x;
+  this.y = y;
+}
 
+Character.prototype.animate = function (imgNos, aniTime, aniMode = "reset") {
+  switch (aniMode) {
+    case "linear":
+      break;
+    case "reset":
+      break;
+    case "reverse":
+      break;
+  }
+}
