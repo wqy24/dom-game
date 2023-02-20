5f9304801f5ee4d0c35a8fc26cd2d4f7be789ffd
Woo, Chyo（伍庆耀） <as2734@126.com>
2023-02-20 20:37:42 +0800

    Add animation (unfinished)

--- dom-game.js
+++ dom-game.js
@@ -4,13 +4,31 @@ function Character(imgs, x, y) {
   $("body").append(this.elem);
   this.x = x;
   this.y = y;
+  this.ani = []
+  this.isAni = false;
 }
 
-Character.prototype.animate = function (imgNos, aniTime, aniMode = "reset") {
+Character.prototype.animate = function (imgNos, aniTime, aniMode = "reset", force = false) {
+  var aniLen = aniTime / imgNos.length;
+  
+  //do not break a running animation
+  if (!force && this.isAni) return false;
+  
+  this.isAni = true;
+  setTimeout(() => {
+    this.isAni = false
+  }, aniTime);
+  
   switch (aniMode) {
     case "linear":
+      for (i in imgNos) {
+        this.ani.push(setTimeout(() => {
+          this.elem.attr();
+        }, i * aniLen));
+      }
       break;
     case "reset":
+      imgNos
       break;
     case "reverse":
       break;
